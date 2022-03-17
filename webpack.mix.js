1a777c6847355251f8fc5385b2c12764a9cf1fb0
anyushu <suzuki@lamp.jp>
2022-03-17 10:45:17 +0900

    tailwindcss の追加

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,12 @@
 const mix = require('laravel-mix')
 
+const is_prod = mix.inProduction() ? true : false
+
 mix
   .options({
-    processCssUrls: false,
+    processCssUrls: is_prod,
   })
   .js('src/js/app.js', 'dist/js')
   .sass('src/scss/app.scss', 'dist/css')
+  .postCss('src/css/app.css', 'dist/css', [require('tailwindcss')])
   .copyDirectory('src/images', 'dist/images')
