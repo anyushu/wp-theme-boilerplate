7263aaa0e0d41d835b20ed1b60608e5ed8298c2a
anyushu <suzuki@lamp.jp>
2022-03-28 18:17:18 +0900

    install phpcsfixer and run phpcsfix

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
 <section class="md:mt-0 mt-12 prose prose-p:m-0 prose-headings:m-0">
-    <?php if (is_active_sidebar('sidebar')) {
-        dynamic_sidebar('sidebar');
-    } ?>
+    <?php if (is_active_sidebar('sidebar')) : ?>
+        <?php dynamic_sidebar('sidebar'); ?>
+    <?php endif; ?>
 </section>
