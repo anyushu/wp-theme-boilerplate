cee81c4d255ae238441cc9350c9162f6c01ff681
anyushu <suzuki@lamp.jp>
2022-03-31 17:36:15 +0900

    fix style post next page

--- pages/page.php
+++ pages/page.php
@@ -1,7 +1,7 @@
 <div class="py-12">
     <div class="container mx-auto px-3">
         <section class="prose">
-            <?php if (have_posts()) : ?>
+            <?php if (have_posts()) : the_post(); ?>
                 <div id="page-<?php the_ID(); ?>" class="prose">
                     <h1 class="text-3xl mb-6 font-bold"><?php the_title(); ?></h1>
                     <?php the_content(); ?>
